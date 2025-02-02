583bfad0cfc8638db9194ea3d38320e2aebd946c
mehranowsky <mehrnowski@gmail.com>
2025-02-02 13:41:31 +0330

    bashscript edited

--- Recon/Narrow/turtle.sh
+++ Recon/Narrow/turtle.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 HOST=$1
-while read fuzz;
-	code=$(torsocks curl -Ls -o /dev/null -w "%{http_code}\n" "$HOST/$fuzz")
-	if [ ! $code == 403 ];then
-		echo "$fuzz"
+block=true
+num=0
+while $block;
+do
+	code=$(curl -Ls -o /dev/null -w "%{http_code}\n" "$HOST" -L)
+	if [ $code == 200 ];then
+		echo "ممد نبودی ببینییی"
+		printf '\a'		
+		exit
 	fi
-done < raft.txt
+	num=$(($num +1))
+	echo -ne "\r$num"
+	#sleep 1
+done
